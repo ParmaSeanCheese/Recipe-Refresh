1714ce0a7ce69291730e731128a0603a6064688f
Sean <Seanpf12@hotmail.com>
2023-10-28 12:15:55 -0500

    Improved Unit Conversion

--- src/main/java/com/launchcode/recipeproject/data/InstructionRepository.java
+++ src/main/java/com/launchcode/recipeproject/data/InstructionRepository.java
@@ -2,6 +2,8 @@ package com.launchcode.recipeproject.data;
 
 import com.launchcode.recipeproject.models.Instruction;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InstructionRepository extends CrudRepository<Instruction, Integer> {
 }
